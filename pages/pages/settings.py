e333f142c46533e51054a6e9715488419d424707
Nad GarDez <iranad@hotmail.com>
2025-01-30 12:03:01 -0400

    added prod settings

--- pages/pages/settings.py
+++ pages/pages/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#5)%ibe)pg+pr5pf--c0vg#4b_1^c4918h@zrer)$s+(63c&@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
